aad234d3f42856eb1c8b975d361cc0fb068f34cf
Jonas Pollpeter <js.pp@gmx.de>
2023-05-15 21:39:09 +0200

    merges develop rebase into client1_jonas

---
+++ src/main/kotlin/client1/Client1GameLogic.kt
@@ -0,0 +1,32 @@
+package client1
+
+import entity.Card
+
+
+/**
+ * Contains the internal business game logic for the Client1 KI
+ * Provides methods to decide the next moves and game actions and stores game related data
+ * to calculate specific actions.
+ *
+ * @author [Jonas Pollpeter](https://github.com/JonasPTFL)
+ */
+internal class Client1GameLogic {
+
+    /**
+     * Finds all card sets of the same color, which contain enough cards to discard a specific part of them
+     * @param currentDiscardPileCard current first discard pile card
+     * @param hand hand of the client player
+     * */
+    fun getDiscardableCards(currentDiscardPileCard: Card, hand: List<Card>): List<List<Card>> {
+        return currentDiscardPileCard.colors.mapNotNull { requiredColor ->
+            hand.filter { handCard ->
+                // filter hand by cards matching the required color
+                handCard.colors.contains(requiredColor)
+            }.takeIf { setCandidate ->
+                // filter card-set-candidates that matches the required amount of card, which is
+                // given by the number value of the current discard pile
+                setCandidate.size >= currentDiscardPileCard.value
+            }
+        }
+    }
+}
