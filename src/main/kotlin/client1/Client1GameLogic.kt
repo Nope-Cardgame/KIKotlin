3fbf5f4405fddfb639e3a97dbfc9c7a0de7b97ed
Jonas <js.pp@gmx.de>
2023-05-25 09:32:43 +0200

    sometimes working action cards game, fixes error in invisible card sublist

--- src/main/kotlin/client1/Client1GameLogic.kt
+++ src/main/kotlin/client1/Client1GameLogic.kt
@@ -1,7 +1,9 @@
 package client1
 
 import entity.Card
+import entity.CardColor
 import entity.CardType
+import java.lang.IllegalStateException
 
 
 /**
@@ -15,21 +17,57 @@ internal class Client1GameLogic {
 
     /**
      * Finds all card sets of the same color, which contain enough cards to discard a specific part of them
-     * @param currentDiscardPileCard current first discard pile card
+     * @param currentDiscardPile current discard pile list
      * @param hand hand of the client player
      * */
-    fun getDiscardableNumberCards(currentDiscardPileCard: Card, hand: List<Card>): List<List<Card>> {
-        return currentDiscardPileCard.colors.mapNotNull { requiredColor ->
+    fun getDiscardableNumberCards(currentDiscardPile: List<Card>, hand: List<Card>): List<List<Card>> {
+        if (currentDiscardPile.isEmpty()) return emptyList()
+
+        val currentDiscardPileCard = currentDiscardPile[0]
+
+        // check if the discard pile contains just the start card
+        if (currentDiscardPile.size == 1) {
+            return when(currentDiscardPileCard.type) {
+                CardType.NUMBER -> {
+                    // card value must exist for number cards checked by this case
+                    findDiscardableCardSet(currentDiscardPileCard.colors, currentDiscardPileCard.value!!, hand)
+                }
+                CardType.NOMINATE -> throw IllegalStateException("nominate card should not be detected in getDiscardableNumberCards")
+                CardType.RESET -> hand.filter { it.type == CardType.NUMBER }.map { listOf(it) }
+                // recursively get discardable cards for previous card on the discard pile (card before invisible action card)
+                CardType.INVISIBLE -> findDiscardableCardSet(currentDiscardPileCard.colors, 1, hand)
+            }
+        } else {
+            return when(currentDiscardPileCard.type) {
+                CardType.NUMBER -> {
+                    // card value must exist for number cards checked by this case
+                    findDiscardableCardSet(currentDiscardPileCard.colors, currentDiscardPileCard.value!!, hand)
+                }
+                CardType.NOMINATE -> throw IllegalStateException("nominate card should not be detected in getDiscardableNumberCards")
+                CardType.RESET -> hand.filter { it.type == CardType.NUMBER }.map { listOf(it) }
+                // recursively get discardable cards for previous card on the discard pile (card before invisible action card)
+                CardType.INVISIBLE -> getDiscardableNumberCards(currentDiscardPile.subList(1,currentDiscardPile.size), hand)
+            }
+        }
+    }
+
+    fun findDiscardableCardSet(colors: List<CardColor>, amount: Int, hand: List<Card>): List<List<Card>> {
+        return colors.mapNotNull { requiredColor ->
             hand.filter { handCard ->
                 // filter hand by cards matching the required color
                 handCard.type == CardType.NUMBER && handCard.colors.contains(requiredColor)
             }.takeIf { setCandidate ->
                 // filter card-set-candidates that matches the required amount of card, which is
                 // given by the number value of the current discard pile
-                setCandidate.size >= currentDiscardPileCard.value
-            }
+                setCandidate.size >= amount
+            }?.take(
+                // TODO implement logic, that discards specific cards for a good reason and not just discard
+                //  the first valid card set
+                amount
+            )
         }
     }
+
     /**
      * Finds playable action card
      * */
