46d920e5229aabe872d334deb2b4f6c14309173b
Jonas Pollpeter <js.pp@gmx.de>
2023-05-31 12:34:25 +0200

    fixes invalid Player.cardAmount for player objects on game start - adds missing options for tournament and game start

--- src/main/kotlin/entity/StartGamePostData.kt
+++ src/main/kotlin/entity/StartGamePostData.kt
@@ -8,5 +8,8 @@ data class StartGamePostData(
     val noActionCards: Boolean,
     val noWildCards: Boolean,
     val oneMoreStartCards: Boolean,
+    val actionTimeout: Int, // 1-120 seconds
+    val invitationTimeout: Int, // 1-600 seconds
+    val startWithRejection: Boolean,
     val players: List<Player>
 )
